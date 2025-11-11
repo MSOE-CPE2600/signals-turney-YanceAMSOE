e8f5dd57c28e67a0da0556d377796b78935172ca
Aden Yance <yancea@msoe.edu>
2025-11-10 19:17:25 -0600

    Finished Lab

--- recv_signal.c
+++ recv_signal.c
@@ -1,6 +1,53 @@
 /**
- * File: 
- * Modified by:
+ * File: recv_signal.c
+ * Description: Receives SIGUSR1 signals with data using sigaction
+ *              The handler retrieves and prints the integer value sent via sigqueue
+ * 
+ * Modified by: Aden Yance
  * 
  * Brief summary of program:
+ * - Uses sigaction() with SA_SIGINFO flag to register a handler for SIGUSR1
+ * - Handler receives siginfo_t structure containing the data sent via sigqueue
+ * - Retrieves and prints the sival_int value from the signal data
+ * - Waits in an infinite loop for signals to be received
+ * 
+ * Compile: gcc -o recv_signal recv_signal.c
+ * 
+ * Usage: Run this program first, note the PID, then run send_signal with that PID
  */
+
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void handle_sigusr1(int sig, siginfo_t *info, void *context) {
+    printf("SIGUSR1 received from process ID: %d\n", info->si_pid);
+    printf("Received integer value: %d\n", info->si_value.sival_int);
+}
+
+int main() {
+    struct sigaction sa;
+    
+    // Initialize the sigaction structure
+    sa.sa_sigaction = handle_sigusr1;  // Use sa_sigaction for three-parameter handler
+    sigemptyset(&sa.sa_mask);           // Clear the signal mask
+    sa.sa_flags = SA_SIGINFO;           // Use SA_SIGINFO flag to receive signal data
+    
+    // Register the signal handler using sigaction
+    if (sigaction(SIGUSR1, &sa, NULL) == -1) {
+        perror("sigaction");
+        exit(1);
+    }
+    
+    printf("Receiver Process ID: %d\n", getpid());
+    printf("Waiting for SIGUSR1 signal with data...\n");
+    printf("Run send_signal with this PID: %d\n", getpid());
+    
+    // Wait in an infinite loop for signals
+    while(1) {
+        pause();  // Wait for any signal
+    }
+    
+    return 0;
+}
--- send_signal.c
+++ send_signal.c
@@ -1,6 +1,65 @@
 /**
- * File: 
- * Modified by:
+ * File: send_signal.c
+ * Description: Sends SIGUSR1 signal with data using sigqueue
+ *              Generates a random integer and sends it to a target process
+ * 
+ * Modified by: Aden Yance
  * 
  * Brief summary of program:
+ * - Takes target process ID as command line argument
+ * - Seeds random number generator with current time
+ * - Generates a random integer
+ * - Prints the random number
+ * - Uses sigqueue() to send SIGUSR1 with the random integer as data
+ * Usage: ./send_signal <target_pid>
+ *        Example: ./send_signal 12345
  */
+
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+int main(int argc, char *argv[]) {
+    pid_t target_pid;
+    int random_value;
+    union sigval value;
+    
+    // Check command line arguments
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <target_pid>\n", argv[0]);
+        fprintf(stderr, "Example: %s 12345\n", argv[0]);
+        exit(1);
+    }
+    
+    // Convert command line argument to process ID
+    target_pid = (pid_t)atoi(argv[1]);
+    
+    if (target_pid <= 0) {
+        fprintf(stderr, "Error: Invalid process ID\n");
+        exit(1);
+    }
+    
+    // Seed random number generator with current time
+    srand((unsigned int)time(NULL));
+    
+    // Generate random integer
+    random_value = rand();
+    
+    // Prepare the value to send with the signal
+    value.sival_int = random_value;
+    
+    // Print the random number before sending
+    printf("Sending SIGUSR1 to process %d with random integer: %d\n", target_pid, random_value);
+    
+    // Send SIGUSR1 with data using sigqueue
+    if (sigqueue(target_pid, SIGUSR1, value) == -1) {
+        perror("sigqueue");
+        exit(1);
+    }
+    
+    printf("Signal sent successfully\n");
+    
+    return 0;
+}
