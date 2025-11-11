7c634fc1ad1bf4b953f0d481e10ecfa0576580a3
Aden Yance <yancea@msoe.edu>
2025-11-10 18:03:29 -0600

    Finished Segfault

--- signal_segfault.c
+++ signal_segfault.c
@@ -1,36 +1,40 @@
 /**
- * @file signal_segfault.c
- * @brief Program that dereferences a null pointer causing a segmentation fault
- */
-
-/**
- * Modified by:
+ * file: signal_segfault.c
+ * Description: Program that demonstrates SIGSEGV signal handling
  * 
- * Brief summary of modifications:
+ * Modified by: Aden Yance
+ * 
+ *   Modifications:
+ * - Installed a signal handler for SIGSEGV
+ * - Handler prints a message when segmentation fault occurs
+ * - Handler returns without performing any corrective action
+ * - This causes execution to continue from the faulting instruction, resulting
+ *   in an infinite loop of segmentation faults
  */
 
- #include <signal.h>
- #include <unistd.h>
- #include <stdlib.h>
- #include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-int main (int argc, char* argv[]) {
 
-    void handle_segfault() {
-        printf("A segmentation fault was received\n");
-        // Return without performing any other action
-        // Execution will continue from where it left off
-    }
+void handle_segfault(int sig) {
+    printf("A segmentation fault (SIGSEGV) was received\n");
+    // Execution will continue from where it left off
+}
 
+int main(int argc, char* argv[]) {
     // Register SIGSEGV signal handler
     signal(SIGSEGV, handle_segfault);
 
     // Declare a null pointer
     int* i = NULL;
 
-    // Dereference the null pointer
+    // Dereference the null pointer - this will trigger SIGSEGV
+    // After the handler returns, execution continues here, causing another SIGSEGV
+    // This creates an infinite loop of segmentation faults
     printf("The value of i is: %d\n", *i);
 
-    // Return to exit the program
+    // This line will never be reached due to the infinite loop of SIGSEGV signals
     return 0;
 }
